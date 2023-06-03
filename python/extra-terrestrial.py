86ce3cb1c897332ceda39e486d1e8f7e90b0d29a
LWRGitHub <LoganReynoldsLWR@gmail.com>
2023-06-03 14:57:17 -0700

    Extra-Terrestrial PY

---
+++ python/extra-terrestrial.py
@@ -0,0 +1,42 @@
+"""
+
+Extra-Terrestrial
+
+
+You meet a group of aliens, and their language is just like English except that they say every word backwards. 
+How will you learn to communicate with them?
+
+Task: 
+Take a word in English that you would like to say, and turn it into language that these aliens will understand.
+
+Input Format: 
+A string of a word in English.
+
+Output Format: 
+A string of the reversed word that represents the original word translated into alien language.
+
+Sample Input: 
+howdy
+
+Sample Output: 
+ydwoh
+
+
+Explanation: 
+If you flip howdy backwards you get ydwoh. 
+
+
+SoloLearn (2023) Extra-Terrestrial: https://www.sololearn.com
+
+"""
+
+
+english = str(input())
+word_reversed = ''
+
+for c in reversed(english):
+	word_reversed += c
+	
+print(word_reversed)
+
+	
