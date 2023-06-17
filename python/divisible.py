cc1468108c653a4a6e90eb3ac5f8264d74d96dea
LWRGitHub <53380788+LWRGitHub@users.noreply.github.com>
2023-06-17 11:01:36 -0700

    Divisible PY

---
+++ python/divisible.py
@@ -0,0 +1,45 @@
+"""
+
+Divisible
+
+
+You need to know if a number is divisible by each of a group of other numbers. If you are given the number and the group of other numbers, you will test to make sure that it is divisible by all of them.  
+
+Task: 
+Test your number against all of the other numbers that you are given to make sure that it is divisible by them.
+
+Input Format: 
+Two inputs: an integer value (the number you are testing) and a string of variable length of the integers that you should test against separated by spaces.
+
+Output Format: 
+A string that says 'divisible by all' or 'not divisible by all'.
+
+Sample Input: 
+100
+2 5 10
+
+Sample Output: 
+divisible by all
+
+
+Explanation: 
+100 is divisible by 2, 5, and 10. 
+
+
+SoloLearn (2023) Divisible: https://www.sololearn.com
+
+"""
+
+
+def divisible_check(main_num, nums):
+
+	for num in nums:
+		if main_num % int(num) != 0:
+			return 'not divisible by all'
+			
+	return 'divisible by all'
+		
+print(divisible_check(int(input()), str(input()).split(' ')))
+
+	
+	
