ed8640f50ea2519b8eaa0f40aacdab9ce43d3710
LWRGitHub <53380788+LWRGitHub@users.noreply.github.com>
2023-07-01 16:10:15 -0700

    Taxes SQL

---
+++ sql/taxes.sql
@@ -0,0 +1,42 @@
+/* SQL: SoloLearn
+ 
+
+Taxes  
+ 
+You are working on the Employees table, which stores the names and salaries of employees. 
+You need to calculate the taxes for the salaries and output them as a new column. 
+The tax percentage is based on the salary amount: 
+ 
+ • 0 - 1500:       10% 
+ • 1501 - 2000: 20% 
+ • 2001+:          30% 
+ 
+ 
+Output the firstname, lastname, salary and tax columns of the table, sorted by the lastname column in ascending order. 
+
+
+Hint: To calculate the percentage of a number, simply multiply it by the percentage divided by 100. For example, to get 10%, multiply the number by 0.1.
+
+
+SoloLearn (2023) Taxes: https://www.sololearn.com
+
+*/
+
+
+SELECT
+	firstname,
+	lastname,
+	salary,
+	CASE
+		WHEN salary < 1501 
+			THEN salary * .1
+		WHEN salary < 2001
+			THEN salary * .2
+		ELSE 
+			salary * .3 
+	END AS tax
+FROM Employees
+ORDER BY lastname ASC;
+
+	
+	
