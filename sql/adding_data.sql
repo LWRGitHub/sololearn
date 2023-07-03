95756501baecbdbd98990354b28adf13cc7a3deb
LWRGitHub <53380788+LWRGitHub@users.noreply.github.com>
2023-07-03 08:45:34 -0700

    Adding Data SQL

---
+++ sql/adding_data.sql
@@ -0,0 +1,35 @@
+/* SQL: SoloLearn
+ 
+
+Adding Data  
+ 
+There are new employees that need to be added to the Employees table. 
+Here is their data: 
+Firstname: Wang 
+Lastname: Lee 
+Salary: 1900 
+ 
+Firstname: Greta 
+Lastname: Wu 
+Salary: 1200 
+ 
+The Employees table has an identity column called id, which is set to AUTO_INCREMENT. 
+Add the data to the table, then select the id, firstname, lastname and salary columns sorted by the id column in descending order.
+
+
+SoloLearn (2023) Adding Data: https://www.sololearn.com
+
+*/
+
+
+INSERT INTO Employees (firstname, lastname, salary)
+VALUES 
+	('Wang', 'Lee', 1900),
+	('Greta', 'Wu', 1200);
+
+SELECT id, firstname, lastname, salary 
+FROM Employees
+ORDER BY id DESC;
+
+	
+	
