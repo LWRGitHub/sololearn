124709ca53a90a78d3fae6f8ca68afb94e11816a
LWRGitHub <53380788+LWRGitHub@users.noreply.github.com>
2023-07-11 14:43:30 -0700

    Number Of Ones PY

---
+++ python/number_of_ones.py
@@ -0,0 +1,33 @@
+""" PY: SoloLearn 
+
+Number Of Ones
+
+Task:
+Count the number of ones in the binary representation of a given integer.
+
+Input Format:
+An integer.
+
+Output Format: 
+An integer representing the count of ones in the binary representation of the input.
+
+Sample Input:
+9
+
+Sample Output:
+2 
+
+
+Explanation: 
+The binary representation of 9 is 1001, which includes 2 ones. 
+
+
+SoloLearn (2023) Number Of Ones: https://www.sololearn.com 
+
+"""
+
+print(bin(int(input())).count('1'))
+
+
+	
+	
